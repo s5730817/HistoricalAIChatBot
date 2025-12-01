2256fefe934b77c4b233ff00c7c7fce67a8e61e4
s5730817 <s5730817@bournemouth.ac.uk>
2025-12-01 20:56:30 +0000

    Enhance chat functionality with message processing and simple response generation

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,44 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+var fs = require("fs"), json;
+
+function readJsonFile(filepath, encoding) {
+    if (encoding === "undefined") {
+        encoding = "utf-8";
+    }
+    var file = fs.readFileSync(filepath, encoding);
+    return JSON.parse(file);
+}
+
+const data = readJsonFile("../Leo.json", "utf-8");
+
+
 app.post("/api/chat", (req, res) => {
-    const userMessage = req.body.message;
+    let botResponse = "Could not find appropriate response";
+    
+    // Get message from the request body, 
+    // make everything lowercase and get rid of non-letter chars
+    const userMessage = req.body.message.toLowerCase().replace(/[^a-zA-Z]/g, "");
+
+    // For debugging
+    console.log("MESSAGE: " + userMessage);
+    
+    // Separate message into words, look for specific intent of
+    // message and give appropriate response
+    const words = userMessage.split(" ");
+    for (const category of data.categories) {
+        for (const word of words) {
+            if (category.matchers.includes(word)) {
+                console.log(category.matchers);
+                botResponse = category.responses[Math.floor(Math.random() * category.responses.length)];
+                break;
+            }
+        }
+    }
 
     return res.json({
-        response: `you said ${userMessage}`
+        response: `${botResponse}`
     });
 });
 
